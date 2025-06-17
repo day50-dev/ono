3885bb06946ef8cec6c90f083c410e9a1b514566
Chris McKenzie <kristopolous@yahoo.com>
2025-06-17 15:43:28 -0700

    first real parser

--- implementations/ono.py
+++ implementations/ono.py
@@ -1,43 +1,69 @@
 #!/usr/bin/env python3
 import sys
 import re
-
-tokens = []
-stack = []
-rangeList = []
+from pprint import pprint
 
 with open(sys.argv[1]) as f:
     text = f.read()
 
 def delimiters_match(open, close):
-    map = {'"': '"', 
-           "'": "'", 
-           '{': '}', 
-           '[': ']',
-           '<': '>'
-           }
+    map = {'"': '"', "'": "'", 
+           '{': '}', '[': ']',
+           '<': '>' }
 
-    if open in map:
-        if close == map[open]:
+    if open[0] in map:
+        if close[-1] == map[open[0]]:
             return True
 
-for match in re.finditer(r'(["\'{<])\?ono\b|(\?["\'}>\]])' , text):
-    if match.group(1):  # Opening: "?ono, {?ono, etc.
-        tokens.append([match.start(), 'open', match.group(0)])
-    else:  # Closing: ?", ?}, etc.
-        tokens.append([match.start(), 'close', match.group(0)])
+def find_blocks(text):
+    tokenList = []
 
-print(tokens)
-for offset, token_type, token in tokens:
-    if token_type == 'open':
-        stack.append((offset, token))
-    elif token_type == 'close' and stack:
-        start_offset, opener = stack.pop()
-        if delimiters_match(opener, token):
-            rangeList.append((start_offset, offset + len(token)))
+    for match in re.finditer(r'(["\'{<])\?ono\b|(\?["\'}>\]])' , text):
+        if match.group(1):  # Opening: "?ono, {?ono, etc.
+            tokenList.append([match.start(), 'open', match.group(0)])
+        else:  # Closing: ?", ?}, etc.
+            tokenList.append([match.start(), 'close', match.group(0)])
+    return tokenList
 
-if len(rangeList) == 0:
+def find_ranges(tokenList):
+    stack = []
+    rangeList = []
+
+    for offset, token_type, token in tokenList:
+        if token_type == 'open':
+            stack.append((offset, token))
+        elif token_type == 'close' and stack:
+            start_offset, opener = stack.pop()
+            if delimiters_match(opener, token):
+                rangeList.append((start_offset, start_offset + len(opener), offset, offset + len(token)))
+    return rangeList
+
+def parse_ranges(rangeList, text):
+    onoList = []
+    for r in rangeList:
+        ono = {'pos': r, 'orig': text[r[0]:r[3]], 'kv': {}, 'prompt': []}
+
+        inner = text[r[1]:r[2]]
+
+        for line in inner.split('\n'):
+            iskv = re.match(r'^\s*([^\s]+)\s*=\s*([^\s]+)\s*$', line)
+            if iskv:
+                key, value = iskv.groups()
+                ono['kv'][key] = value.strip('"\'')
+            else:
+                ono['prompt'].append(line)
+
+        ono['prompt'] = "\n".join(ono['prompt']).strip()
+        onoList.append(ono)
+    return onoList
+
+blockList = find_blocks(text)
+rangeList = find_ranges(blockList)
+onoList = parse_ranges(rangeList, text)
+
+
+if len(onoList) == 0:
     print(f"Nothing found for {text}")
 
-for ra in rangeList:
-    print(ra)
+for ono in onoList:
+    pprint(ono)
